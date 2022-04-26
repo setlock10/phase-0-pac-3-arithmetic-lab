17cf3048de974e4e604caec6ffb9e95cddeb2630
John J Setlock II <johnsetlock@gmail.com>
2022-04-26 14:58:34 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,40 @@
+function add(a,b) {
+    // we'll fill this in shortly
+    return a+b;
+  }
+
+  function subtract(a,b) {
+    // we'll fill this in shortly
+    return a-b;
+  }
+
+  function multiply(a,b) {
+    // we'll fill this in shortly
+    return a*b;
+  }
+
+  function divide(a,b) {
+    // we'll fill this in shortly
+    return a/b;
+  }
+
+  function increment(n){
+      n++;
+    return n;
+}
+
+function decrement(n){
+    n--;
+    return n;
+}
+
+function makeInt(string){
+   return parseInt(string,10);
+}
+function preserveDecimal(n){
+    return parseFloat(n);
+}
+
+
+
+  
